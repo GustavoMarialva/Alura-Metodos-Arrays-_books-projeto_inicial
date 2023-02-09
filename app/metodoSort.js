46cfd7560112b0a4dc58422c04d0cb429a82c3c0
Gustavo Marialva <gmarialva@gmail.com>
2023-02-09 15:49:01 -0300

    adiciona sort por preço

---
+++ app/metodoSort.js
@@ -0,0 +1,7 @@
+const btnOrdenarPreco = document.getElementById("btnOrdenarPorPreco");
+btnOrdenarPreco.addEventListener("click", ordernarLivrosPorPreco);
+
+function ordernarLivrosPorPreco() {
+  let livrosOrdernados = livros.sort((a, b) => a.preco - b.preco);
+  exibirOsLivrosNaTela(livrosOrdernados);
+}
